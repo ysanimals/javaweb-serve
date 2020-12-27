6efaa5a4e2c40ce62960c1a7ad12b0eb11d455f4
ysanimals <71752148+ysanimals@users.noreply.github.com>
2020-12-27 10:59:11 +0800

    第一个版本

--- src/main/java/com/nit/ssm/dto/UserDTO.java
+++ src/main/java/com/nit/ssm/dto/UserDTO.java
@@ -11,24 +11,18 @@ public class UserDTO {
     private Integer userType;
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     private Date gmtCreate;
-
-    public Date getGmtCreate() {
-        return gmtCreate;
-    }
-
-    public void setGmtCreate(Date gmtCreate) {
-        this.gmtCreate = gmtCreate;
-    }
+    private Integer key;
 
     public UserDTO() {
     }
 
-    public UserDTO(Integer userId, String userName, String userPwd, Integer userType,Date gmtCreate) {
+    public UserDTO(Integer userId, String userName, String userPwd, Integer userType, Date gmtCreate, Integer key) {
         this.userId = userId;
         this.userName = userName;
         this.userPwd = userPwd;
         this.userType = userType;
         this.gmtCreate = gmtCreate;
+        this.key = key;
     }
 
     public Integer getUserId() {
@@ -61,5 +55,21 @@ public class UserDTO {
 
     public void setUserType(Integer userType) {
         this.userType = userType;
+    }
+
+    public Date getGmtCreate() {
+        return gmtCreate;
+    }
+
+    public void setGmtCreate(Date gmtCreate) {
+        this.gmtCreate = gmtCreate;
+    }
+
+    public Integer getKey() {
+        return key;
+    }
+
+    public void setKey(Integer key) {
+        this.key = key;
     }
 }
--- src/main/java/com/nit/ssm/mapper/UserMapper.java
+++ src/main/java/com/nit/ssm/mapper/UserMapper.java
@@ -1,4 +1,12 @@
 package com.nit.ssm.mapper;
 
+
+import com.nit.ssm.dto.UserDTO;
+import com.nit.ssm.entity.UserEntity;
+import org.apache.catalina.User;
+import org.apache.ibatis.annotations.*;
+
+import java.util.List;
 public interface UserMapper {
+//    @Select("SELECT *,user_id AS 'key' from   ")
 }
